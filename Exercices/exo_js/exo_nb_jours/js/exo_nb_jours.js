c54267765272b6b45e35b681a50618eebc6e8962
Unknown <julien.ledoux@lepoles.com>
2017-07-18 21:27:43 +0200

    home

--- Exercices/exo_js/exo_nb_jours/js/exo_nb_jours.js
+++ Exercices/exo_js/exo_nb_jours/js/exo_nb_jours.js
@@ -1,11 +1,11 @@
-var mois = parseInt(prompt("Saisie le mois"));
+var mois = parseInt(prompt("Saisie le numéro du mois de ton choix."));
 
-    if(){
-
-    } else if(){
-
-    } else if (){
-
+    if((mois === 1) || (mois === 3) || (mois === 5) || (mois === 7) || (mois === 8) || (mois === 10) || (mois === 12)){
+        document.write("Il y a 31 jours le " + mois + "eme mois");
+    } else if(mois === 4) || (mois === 6) || (mois === 9) || (mois === 11){
+        document.write("Il y a 30 jours au mois de " + mois + "eme mois");
+    } else if (mois === 2){
+        document.write("Il y a 28 jours au mois de " + mois + "eme mois");
     } else {
-        
+        document.write("Error !");
     }
